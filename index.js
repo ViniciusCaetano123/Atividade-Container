f0c71a1fd400666253275ce03b8ecfdc75a097fb
unknown <vinicaetano.f@gmail.com>
2021-11-20 20:43:22 -0300

    update

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
+app.get('/vem', (req, res) => {
+    res.send('Hello World2');
+  });
 
 app.listen(PORT);
 console.log("Running on http://:${PORT}");
